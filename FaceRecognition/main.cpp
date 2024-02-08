18a238954a378a2dc370f2b5bcef28559a6bbe30
Yongjun <byj5595@naver.com>
2024-02-08 13:52:30 +0900

    ONNX 모델파일 사용 시도

--- FaceRecognition/main.cpp
+++ FaceRecognition/main.cpp
@@ -1,11 +1,14 @@
 #include "FaceRecognition.h"
 #include "CameraWidget.h"
 #include <QtWidgets/QApplication>
+#include "LoadDnnModel.h"
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
     CameraWidget mainwindow;
     mainwindow.show();
+    LoadDnnModel sample;
+    sample.LoadOnnx();
     return app.exec();
 }
